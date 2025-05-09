dca48cbe1a0df8fa9f8ca4da556cbcb1e3111619
CLTowson <145373452+CLTowson@users.noreply.github.com>
2025-05-09 16:10:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Home from "./home";
 import VolunteerForm from "./VolunteerForm";
 import ThankYou from "./ThankYou";
 import AnimalAdoptionForm from "./AnimalAdoptionForm";
+import PetLanding from "./PetLanding";
+import productListings from "./productListings";
+import confirmation from "./confirmation";
+import sell from "./sell";
+import buy from "./buy";
 
 function App() {
   return (
@@ -14,6 +19,11 @@ function App() {
         <Route path="/volunteer" element={<VolunteerForm />} />
         <Route path="/adopt" element={<AnimalAdoptionForm />} />
         <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/PetLanding" element={<PetLanding />} />
+        <Route path="/productListings" element={<productListings />} />
+        <Route path="/confirmation" element={<confirmation />} />
+        <Route path="/sell" element={<sell />} />
+        <Route path="/buy" element={<buy />} />
       </Routes>
     </Router>
   );

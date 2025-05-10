bdea2f63ab0f6bc777268972b9b197d192e9c498
Mario Scotto <115577140+Mario-Scotto@users.noreply.github.com>
2025-05-10 18:40:45 -0400

    Use process.env.PORT for compatibility with Render

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const Product = require('./models/Product');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
